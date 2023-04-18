37a278a50a6d22501c0c0b95f21de79c468fcfa0
Pranav Gupta <88652820+pranavgupta2603@users.noreply.github.com>
2023-04-18 19:10:03 +0530

    changing getting history files

---
+++ historygenerator/using_browser_history_library.py
@@ -0,0 +1,15 @@
+import browserhistory as bh
+import pandas as pd
+from datetime import datetime
+
+browserhistory = bh.get_browserhistory()
+his = browserhistory["chrome"]
+df = {"url":[], "title":[], "last_visit_time":[]}
+
+
+for i in his:
+    df["url"].append(i[0])
+    df["title"].append(i[1])
+    df["last_visit_time"].append(datetime.strptime(i[2], "%Y-%m-%d %H:%M:%S"))
+df = pd.DataFrame(df)
+df.to_csv('../new_his.csv', index=False)
